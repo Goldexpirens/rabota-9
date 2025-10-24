f2110c19a8c8a8158c4f4f5dcb2914a06fcce62c
KeaZ <oaganaov@gmail.com>
2025-10-24 10:35:28 +0300

    Add files via upload

---
+++ ConsoleApplication3/ConsoleApplication3/ConsoleApplication3.cpp
@@ -0,0 +1,26 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(0, "");
+
+	int n;
+	int i=1;
+	cin >> n;
+	while (i<n+1) {
+		cout << "Цикл отработал. Круг:"<<i<<endl;
+		i = i + 1;
+	}
+
+
+	
+
+    
+	
+
+	return 0;
+}
+
